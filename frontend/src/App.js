04b265a623a48173c88937c41ca26b097b4cdfa4
Mundkar <96167282+Mundkar@users.noreply.github.com>
2022-08-25 22:59:33 +0530

    frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import LogInPage from "./Pages/LogInPage/LogInPage";
+// import SignUpPage from "./Pages/SignUpPage/SignUpPage";
+
+function App() {
+  return (
+    <div className="App">
+      <LogInPage />
+      {/* <SignUpPage /> */}
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/Components/LogInSignUpNavbar/LogInSignUpNavbar.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./LogInSignUpNavbar.css";
+
+const LogInSignUpNavbar = () => {
+  return (
+    <div className="loginsignupnavbar">
+      <img
+        src="https://s1.thcdn.com/enterprise/assets/dermstore-global-a3ce7f42-4a2c-4cc5-ad05-ba594900fa02-logo-default.svg"
+        alt="dermstore-logo"
+      />
+    </div>
+  );
+};
+
+export default LogInSignUpNavbar;
