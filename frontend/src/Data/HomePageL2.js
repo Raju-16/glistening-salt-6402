d87951557f699d2faeafe80b4dcfde79307fcb13
Rajuprajapti <rprajuprajapati16@gmail.com>
2022-08-25 15:48:51 +0530

    commit by raju

--- frontend/src/Data/HomePageL2.js
+++ frontend/src/Data/HomePageL2.js
@@ -1,23 +1,58 @@
 export const HomePageL2 = [
   {
-    "imageUrl":
+    "id":"1",
+    imageUrl:
       "https://static.thcdn.com/images/small/webp//productimg/original/13927356-2104980626498655.jpg",
-    "title": "SkinCeuticals Anti-Aging Hyaluronic Acid Set",
-    "Rating": "🌟4.5",
-    "Price": "$32",
+    title: "SkinCeuticals Anti-Aging Hyaluronic Acid Set",
+    Rating: "🌟4.5",
+    Price: "$212",
   },
   {
-    "imageUrl":
+    "id":"2",
+    imageUrl:
       "https://static.thcdn.com/images/small/webp//productimg/original/13927359-1664980626572716.jpg",
-    "title": "SkinCeuticals Anti-Aging Hyaluronic Acid Set with Retinol",
-    "Rating": "🌟4.5",
-    "Price": "$32",
+    title: "SkinCeuticals Anti-Aging Hyaluronic Acid Set with Retinol",
+    Rating: "🌟4.5",
+    Price: "$286",
   },
   {
-    "imageUrl":
+    "id":"3",
+    imageUrl:
       "https://static.thcdn.com/images/small/webp//productimg/original/13927360-5564980626629719.jpg",
-    "title": "skin care",
-    "Rating": "🌟4.5",
-    "Price": "$32",
+    title: "SkinCeuticals Firm and Hydrate Routine",
+    Rating: "🌟4.2",
+    Price: "$265",
+  },
+  {
+    "id":"4",
+    imageUrl:
+      "https://static.thcdn.com/images/small/webp//productimg/original/13927360-5564980626629719.jpg",
+    title: "SkinCeuticals Firm and Hydrate Routine",
+    Rating: "🌟4.3",
+    Price: "$255",
+  },
+  {
+    "id":"5",
+    imageUrl:
+      "https://static.thcdn.com/images/large/webp//productimg/1600/1600/11290635-1804892767902966.jpg",
+    title: "SkinCeuticals Triple Lipid Restore 242",
+    Rating: "🌟4.1",
+    Price: "$138",
+  },
+  {
+    "id":"6",
+    imageUrl:
+      "https://static.thcdn.com/images/large/webp//productimg/1600/1600/11705713-2284962240533307.jpg",
+    title: "SkinCeuticals Hyaluronic Acid Intensifier",
+    Rating: "🌟4.0",
+    Price: "$106",
+  },
+  {
+    "id":"7",
+    imageUrl:
+      "https://static.thcdn.com/images/large/webp//productimg/1600/1600/11289619-2044892771629386.jpg",
+    title: "SkinCeuticals Hydrating B5 Gel ",
+    Rating: "🌟4.3",
+    Price: "$86",
   },
 ];
