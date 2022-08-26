1bd9bb7ab5dc8fb838891fea133525624753979d
Mundkar <96167282+Mundkar@users.noreply.github.com>
2022-08-26 16:54:06 +0530

    products-running - venkatesh

--- frontend/src/Redux/store.js
+++ frontend/src/Redux/store.js
@@ -1,13 +1,3 @@
-// import { applyMiddleware, legacy_createStore } from "redux";
-// import { composeWithDevTools } from "redux-devtools-extension";
-// import thunk from "redux-thunk";
-// import { reducer } from "./UserReducer/reducer";
-
-// export const store = legacy_createStore(
-//   reducer,
-//   composeWithDevTools(applyMiddleware(thunk))
-// );
-
 import {
   applyMiddleware,
   combineReducers,
