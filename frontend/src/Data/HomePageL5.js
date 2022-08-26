d51fb131d64dc500e6795c827cd51c6fe03068f8
Rajuprajapti <rprajuprajapati16@gmail.com>
2022-08-26 12:02:26 +0530

    add some data

---
+++ frontend/src/Data/HomePageL5.js
@@ -0,0 +1,24 @@
+export const HomePageL5 = [
+  {
+    id: "1",
+    imageUrl:
+      "https://static.thcdn.com/images/small/webp/widgets/208-us/21/0801-STDCRE-39436-DS-JH-Aug_Batching_-_Second_Priority-Shot_20-HP_LPSQ_BANNER_676x556-044421.jpg",
+    title: "New from SENTÉ",
+    detail: "A hydroquinone-free formula that improves the look of dark spots.",
+  },
+  {
+    id: "2",
+    imageUrl:
+      "https://static.thcdn.com/images/small/webp/widgets/208-us/29/0808-STDCRE-39815-CM-DS-Shot1-2-HP_LPSQ_BANNER_676x556-091329.jpg",
+    title: "shop our Anniversary Roundup",
+    detail: "Our tested, tried & true picks in beauty, skin & hair care.",
+  },
+  {
+    id: "3",
+    imageUrl:
+      "https://static.thcdn.com/images/small/webp/widgets/208-us/35/HP_LPSQ_BANNER_676x556-044335.jpg",
+    title: "New from Dr. Dennis Gross",
+    detail:
+      "Promotes visibly firmer, brighter & more even-looking skin while helping to strengthen its moisture barrier.",
+  },
+];
---
+++ frontend/src/Data/HomePageL7.js
@@ -0,0 +1,62 @@
+export const HomePageL7 = [
+  {
+    id: "1",
+    imageUrl:
+      "https://static.thcdn.com/images/small/webp/widgets/208-us/41/Top_Ten_Brands-055041.png",
+    title: "Top Ten brands",
+  },
+  {
+    id: "2",
+    imageUrl:
+      "https://static.thcdn.com/images/small/webp/widgets/208-us/56/Travel_Size-050756.png",
+    title: "Travel Size",
+  },
+  {
+    id: "3",
+    imageUrl:
+      "https://static.thcdn.com/images/small/webp/widgets/208-us/15/Self_Tanner-070615.jpg",
+    title: "Self Tanner",
+  },
+  {
+    id: "4",
+    imageUrl:
+      "https://static.thcdn.com/images/small/webp/widgets/208-us/23/Site_294x368-080123.png",
+    title: "Best Skin care product",
+  },
+  {
+    id: "5",
+    imageUrl:
+      "https://static.thcdn.com/images/small/webp/widgets/208-us/17/HomePageBannerNewRoutineMobile-084945-074617.jpg",
+    title: "Best Retinol Creame",
+  },
+  {
+    id: "6",
+    imageUrl:
+      "https://static.thcdn.com/images/small/webp/widgets/208-us/15/original-eltamd-sb-10-20-090822-081515.jpg",
+    title: "SPF & Sunscreame",
+  },
+  {
+    id: "7",
+    imageUrl:
+      "https://static.thcdn.com/images/small/webp/widgets/208-us/32/Trending-_Build_Your_Own_Routine-082732.png",
+    title: "Build Your Skin Care Routine",
+  },
+  {
+    id: "8",
+    imageUrl:
+      "https://static.thcdn.com/images/small/webp/widgets/208-us/53/SITE_Mobile_600%E2%80%8A%C3%97%E2%80%8A750-080553.jpg",
+    title: "Vitamin C & Antioxident",
+  },
+  {
+    id: "9",
+    imageUrl:
+      "https://static.thcdn.com/images/small/webp/widgets/208-us/04/Wellness_White-061404.png",
+    title: "Self Care & Wellness",
+  },
+  {
+    id: "10",
+    imageUrl:
+      "https://static.thcdn.com/images/small/webp/widgets/208-us/18/Olaplex_trending_white-055218.png",
+    title: "Trending Product",
+  },
+];
