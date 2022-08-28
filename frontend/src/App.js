b32a5bc3c8a9427524aae4ff7d7272a1ae46eb67
Masrur Alam <masrur.jsr@gmail.com>
2022-08-28 10:47:45 +0530

    Footer and Navbar have been added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,14 @@
 import "./App.css";
+import NavbarHeader from "./Components/Navbar/NavbarHeader";
+import Navbar from "./Components/Navbar/Navbar";
+import Footer from "./Components/Footer/Footer";
 
 function App() {
   return (
     <div className="App">
+      <NavbarHeader />
+      <Navbar />
+      <Footer />
     </div>
   );
 }
--- frontend/src/Redux/store.js
+++ frontend/src/Redux/store.js
@@ -1,13 +1,12 @@
-<<<<<<< HEAD
-import { applyMiddleware, legacy_createStore } from "redux";
-// import { composeWithDevTools } from "redux-devtools-extension";
-=======
-import { applyMiddleware, combineReducers, legacy_createStore } from "redux";
-import { composeWithDevTools } from "redux-devtools-extension";
->>>>>>> fcdc57cb925e622a01c020df0e6de46e4ccd12aa
+import {
+  applyMiddleware,
+  combineReducers,
+  compose,
+  legacy_createStore,
+} from "redux";
 import thunk from "redux-thunk";
+import { cartReducer } from "./CartReducer/cartReducer";
 import { userReducer } from "./UserReducer/userReducer";
-import { productReducer } from "./ProductReducer/productReducer";
 
 const reduxDevTool =
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() || compose;
@@ -15,36 +14,6 @@ const reduxDevTool =
 const rootReducer = combineReducers({ cartReducer, userReducer });
 
 export const store = legacy_createStore(
-<<<<<<< HEAD
-  reducer,
-  applyMiddleware(thunk)
-);
-
-// New store
-
-// import {
-//   applyMiddleware,
-//   combineReducers,
-//   compose,
-//   legacy_createStore,
-// } from "redux";
-// import thunk from "redux-thunk";
-// // import { cartReducer } from "./CartReducer/cartReducer";
-// import { userReducer } from "./UserReducer/userReducer";
-
-
-// const reduxDevTool =
-//   window._REDUX_DEVTOOLS_EXTENSION_ && window._REDUX_DEVTOOLS_EXTENSION_();
-// const rootReducer = combineReducers({ userReducer });
-
-
-// export const store = legacy_createStore(
-//   rootReducer,
-//   compose(applyMiddleware(thunk), reduxDevTool)
-// );
-=======
   rootReducer,
   reduxDevTool(applyMiddleware(thunk))
 );
-
->>>>>>> fcdc57cb925e622a01c020df0e6de46e4ccd12aa
